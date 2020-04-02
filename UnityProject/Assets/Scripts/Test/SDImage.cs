8bcb95048e9632565359eaab316928e310c18a9b
zengzhiwei <308164213@qq.com>
2020-04-02 12:48:42 +0800

    * image学习

--- UnityProject/Assets/Scripts/Test/SDImage.cs
+++ UnityProject/Assets/Scripts/Test/SDImage.cs
@@ -194,7 +194,7 @@ public class SDImage : Image
                 GenerateTiledSprite(toFill);
                 break;
             case Type.Filled:
-                //GenerateFilledSprite(toFill, preserveAspect);
+                GenerateFilledSprite(toFill, preserveAspect);
                 break;
         }
     }
@@ -230,6 +230,16 @@ public class SDImage : Image
         vertexHelper.AddVert(new Vector3(posMin.x, posMax.y, 0), color, new Vector2(uvMin.x, uvMax.y));
         vertexHelper.AddVert(new Vector3(posMax.x, posMax.y, 0), color, new Vector2(uvMax.x, uvMax.y));
         vertexHelper.AddVert(new Vector3(posMax.x, posMin.y, 0), color, new Vector2(uvMax.x, uvMin.y));
+
+        vertexHelper.AddTriangle(startIndex, startIndex + 1, startIndex + 2);
+        vertexHelper.AddTriangle(startIndex + 2, startIndex + 3, startIndex);
+    }
+    static void AddQuad(VertexHelper vertexHelper, Vector3[] quadPositions, Color32 color, Vector3[] quadUVs)
+    {
+        int startIndex = vertexHelper.currentVertCount;
+
+        for(int i = 0; i < 4; ++i)
+            vertexHelper.AddVert(quadPositions[i], color, quadUVs[i]);
 
         vertexHelper.AddTriangle(startIndex, startIndex + 1, startIndex + 2);
         vertexHelper.AddTriangle(startIndex + 2, startIndex + 3, startIndex);
@@ -502,6 +512,320 @@ public class SDImage : Image
                 color,
                 new Vector2(uvMax.x, uvMax.y),
                 new Vector2(outer.z, outer.w));
+        }
+    }
+
+    /// <summary>
+    /// Generate vertices for a filled Image.
+    /// </summary>
+
+    static readonly Vector3[] s_Xy = new Vector3[4];
+    static readonly Vector3[] s_Uv = new Vector3[4];
+    void GenerateFilledSprite(VertexHelper toFill, bool preserveAspect)
+    {
+        toFill.Clear();
+
+        if(fillAmount < 0.001f)
+            return;
+
+        Vector4 v = GetDrawingDimensions(preserveAspect);
+        Vector4 outer = activeSprite != null ? UnityEngine.Sprites.DataUtility.GetOuterUV(activeSprite) : Vector4.zero;
+        UIVertex uiv = UIVertex.simpleVert;
+        uiv.color = color;
+
+        float tx0 = outer.x;
+        float ty0 = outer.y;
+        float tx1 = outer.z;
+        float ty1 = outer.w;
+
+        // Horizontal and vertical filled sprites are simple -- just end the Image prematurely
+        if(fillMethod == FillMethod.Horizontal || fillMethod == FillMethod.Vertical)
+        {
+            if(fillMethod == FillMethod.Horizontal)
+            {
+                float fill = (tx1 - tx0) * fillAmount;
+
+                if(fillOrigin == 1)
+                {
+                    v.x = v.z - (v.z - v.x) * fillAmount;
+                    tx0 = tx1 - fill;
+                }
+                else
+                {
+                    v.z = v.x + (v.z - v.x) * fillAmount;
+                    tx1 = tx0 + fill;
+                }
+            }
+            else if(fillMethod == FillMethod.Vertical)
+            {
+                float fill = (ty1 - ty0) * fillAmount;
+
+                if(fillOrigin == 1)
+                {
+                    v.y = v.w - (v.w - v.y) * fillAmount;
+                    ty0 = ty1 - fill;
+                }
+                else
+                {
+                    v.w = v.y + (v.w - v.y) * fillAmount;
+                    ty1 = ty0 + fill;
+                }
+            }
+        }
+
+        s_Xy[0] = new Vector2(v.x, v.y);
+        s_Xy[1] = new Vector2(v.x, v.w);
+        s_Xy[2] = new Vector2(v.z, v.w);
+        s_Xy[3] = new Vector2(v.z, v.y);
+
+        s_Uv[0] = new Vector2(tx0, ty0);
+        s_Uv[1] = new Vector2(tx0, ty1);
+        s_Uv[2] = new Vector2(tx1, ty1);
+        s_Uv[3] = new Vector2(tx1, ty0);
+
+        {
+            if(fillAmount < 1f && fillMethod != FillMethod.Horizontal && fillMethod != FillMethod.Vertical)
+            {
+                if(fillMethod == FillMethod.Radial90)
+                {
+                    if(RadialCut(s_Xy, s_Uv, fillAmount, fillClockwise, fillOrigin))
+                        AddQuad(toFill, s_Xy, color, s_Uv);
+                }
+                else if(fillMethod == FillMethod.Radial180)
+                {
+                    for(int side = 0; side < 2; ++side)
+                    {
+                        float fx0, fx1, fy0, fy1;
+                        int even = fillOrigin > 1 ? 1 : 0;
+
+                        if(fillOrigin == 0 || fillOrigin == 2)
+                        {
+                            fy0 = 0f;
+                            fy1 = 1f;
+                            if(side == even)
+                            {
+                                fx0 = 0f;
+                                fx1 = 0.5f;
+                            }
+                            else
+                            {
+                                fx0 = 0.5f;
+                                fx1 = 1f;
+                            }
+                        }
+                        else
+                        {
+                            fx0 = 0f;
+                            fx1 = 1f;
+                            if(side == even)
+                            {
+                                fy0 = 0.5f;
+                                fy1 = 1f;
+                            }
+                            else
+                            {
+                                fy0 = 0f;
+                                fy1 = 0.5f;
+                            }
+                        }
+
+                        s_Xy[0].x = Mathf.Lerp(v.x, v.z, fx0);
+                        s_Xy[1].x = s_Xy[0].x;
+                        s_Xy[2].x = Mathf.Lerp(v.x, v.z, fx1);
+                        s_Xy[3].x = s_Xy[2].x;
+
+                        s_Xy[0].y = Mathf.Lerp(v.y, v.w, fy0);
+                        s_Xy[1].y = Mathf.Lerp(v.y, v.w, fy1);
+                        s_Xy[2].y = s_Xy[1].y;
+                        s_Xy[3].y = s_Xy[0].y;
+
+                        s_Uv[0].x = Mathf.Lerp(tx0, tx1, fx0);
+                        s_Uv[1].x = s_Uv[0].x;
+                        s_Uv[2].x = Mathf.Lerp(tx0, tx1, fx1);
+                        s_Uv[3].x = s_Uv[2].x;
+
+                        s_Uv[0].y = Mathf.Lerp(ty0, ty1, fy0);
+                        s_Uv[1].y = Mathf.Lerp(ty0, ty1, fy1);
+                        s_Uv[2].y = s_Uv[1].y;
+                        s_Uv[3].y = s_Uv[0].y;
+
+                        float val = fillClockwise ? fillAmount * 2f - side : fillAmount * 2f - (1 - side);
+
+                        if(RadialCut(s_Xy, s_Uv, Mathf.Clamp01(val), fillClockwise, ((side + fillOrigin + 3) % 4)))
+                        {
+                            AddQuad(toFill, s_Xy, color, s_Uv);
+                        }
+                    }
+                }
+                else if(fillMethod == FillMethod.Radial360)
+                {
+                    for(int corner = 0; corner < 4; ++corner)
+                    {
+                        float fx0, fx1, fy0, fy1;
+
+                        if(corner < 2)
+                        {
+                            fx0 = 0f;
+                            fx1 = 0.5f;
+                        }
+                        else
+                        {
+                            fx0 = 0.5f;
+                            fx1 = 1f;
+                        }
+
+                        if(corner == 0 || corner == 3)
+                        {
+                            fy0 = 0f;
+                            fy1 = 0.5f;
+                        }
+                        else
+                        {
+                            fy0 = 0.5f;
+                            fy1 = 1f;
+                        }
+
+                        s_Xy[0].x = Mathf.Lerp(v.x, v.z, fx0);
+                        s_Xy[1].x = s_Xy[0].x;
+                        s_Xy[2].x = Mathf.Lerp(v.x, v.z, fx1);
+                        s_Xy[3].x = s_Xy[2].x;
+
+                        s_Xy[0].y = Mathf.Lerp(v.y, v.w, fy0);
+                        s_Xy[1].y = Mathf.Lerp(v.y, v.w, fy1);
+                        s_Xy[2].y = s_Xy[1].y;
+                        s_Xy[3].y = s_Xy[0].y;
+
+                        s_Uv[0].x = Mathf.Lerp(tx0, tx1, fx0);
+                        s_Uv[1].x = s_Uv[0].x;
+                        s_Uv[2].x = Mathf.Lerp(tx0, tx1, fx1);
+                        s_Uv[3].x = s_Uv[2].x;
+
+                        s_Uv[0].y = Mathf.Lerp(ty0, ty1, fy0);
+                        s_Uv[1].y = Mathf.Lerp(ty0, ty1, fy1);
+                        s_Uv[2].y = s_Uv[1].y;
+                        s_Uv[3].y = s_Uv[0].y;
+
+                        float val = fillClockwise ?
+                            fillAmount * 4f - ((corner + fillOrigin) % 4) :
+                            fillAmount * 4f - (3 - ((corner + fillOrigin) % 4));
+
+                        if(RadialCut(s_Xy, s_Uv, Mathf.Clamp01(val), fillClockwise, ((corner + 2) % 4)))
+                            AddQuad(toFill, s_Xy, color, s_Uv);
+                    }
+                }
+            }
+            else
+            {
+                AddQuad(toFill, s_Xy, color, s_Uv);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Adjust the specified quad, making it be radially filled instead.
+    /// </summary>
+
+    static bool RadialCut(Vector3[] xy, Vector3[] uv, float fill, bool invert, int corner)
+    {
+        // Nothing to fill
+        if(fill < 0.001f) return false;
+
+        // Even corners invert the fill direction
+        if((corner & 1) == 1) invert = !invert;
+
+        // Nothing to adjust
+        if(!invert && fill > 0.999f) return true;
+
+        // Convert 0-1 value into 0 to 90 degrees angle in radians
+        float angle = Mathf.Clamp01(fill);
+        if(invert) angle = 1f - angle;
+        angle *= 90f * Mathf.Deg2Rad;
+
+        // Calculate the effective X and Y factors
+        float cos = Mathf.Cos(angle);
+        float sin = Mathf.Sin(angle);
+
+        RadialCut(xy, cos, sin, invert, corner);
+        RadialCut(uv, cos, sin, invert, corner);
+        return true;
+    }
+
+    /// <summary>
+    /// Adjust the specified quad, making it be radially filled instead.
+    /// </summary>
+
+    static void RadialCut(Vector3[] xy, float cos, float sin, bool invert, int corner)
+    {
+        int i0 = corner;
+        int i1 = ((corner + 1) % 4);
+        int i2 = ((corner + 2) % 4);
+        int i3 = ((corner + 3) % 4);
+
+        if((corner & 1) == 1)
+        {
+            if(sin > cos)
+            {
+                cos /= sin;
+                sin = 1f;
+
+                if(invert)
+                {
+                    xy[i1].x = Mathf.Lerp(xy[i0].x, xy[i2].x, cos);
+                    xy[i2].x = xy[i1].x;
+                }
+            }
+            else if(cos > sin)
+            {
+                sin /= cos;
+                cos = 1f;
+
+                if(!invert)
+                {
+                    xy[i2].y = Mathf.Lerp(xy[i0].y, xy[i2].y, sin);
+                    xy[i3].y = xy[i2].y;
+                }
+            }
+            else
+            {
+                cos = 1f;
+                sin = 1f;
+            }
+
+            if(!invert) xy[i3].x = Mathf.Lerp(xy[i0].x, xy[i2].x, cos);
+            else xy[i1].y = Mathf.Lerp(xy[i0].y, xy[i2].y, sin);
+        }
+        else
+        {
+            if(cos > sin)
+            {
+                sin /= cos;
+                cos = 1f;
+
+                if(!invert)
+                {
+                    xy[i1].y = Mathf.Lerp(xy[i0].y, xy[i2].y, sin);
+                    xy[i2].y = xy[i1].y;
+                }
+            }
+            else if(sin > cos)
+            {
+                cos /= sin;
+                sin = 1f;
+
+                if(invert)
+                {
+                    xy[i2].x = Mathf.Lerp(xy[i0].x, xy[i2].x, cos);
+                    xy[i3].x = xy[i2].x;
+                }
+            }
+            else
+            {
+                cos = 1f;
+                sin = 1f;
+            }
+
+            if(invert) xy[i3].y = Mathf.Lerp(xy[i0].y, xy[i2].y, sin);
+            else xy[i1].x = Mathf.Lerp(xy[i0].x, xy[i2].x, cos);
         }
     }
 
